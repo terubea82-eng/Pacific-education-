960518a5b64ae5025fafd77634d85ff2e740dd2f
TERUBEA TION <terubea82@gmail.com>
2026-09-21 09:27:11 +1200

    Continue Pacific Education build: next-stage control module

---
+++ src/js/pacificEducationOfflineNextStage.js
@@ -0,0 +1,4 @@
+(function(){
+"use strict";
+window.PacificEducationOfflineNextStage=Object.freeze({version:"1.0.0",requirements:Object.freeze({offlineCachedLearning:true,lowBandwidthFallback:true,queuedProgressSync:true,noSecretsInClientCache:true}),status:function(){return {ready:true,productionApproved:false,realDeviceNetworkTestingRequired:true};}});
+})();
