725d4f37f2d69c4c07af375d8275b6604714cc5f
TERUBEA TION <terubea82@gmail.com>
2026-09-21 09:27:02 +1200

    Continue Pacific Education build: next-stage control module

---
+++ src/js/pacificEducationCurriculumAlignmentNextStage.js
@@ -0,0 +1,8 @@
+(function(){
+  "use strict";
+  window.PacificEducationCurriculumAlignmentNextStage=Object.freeze({
+    version:"1.0.0", authority:"Fiji Ministry of Education published curriculum sources",
+    rules:Object.freeze({officialSourceRequired:true, achievementIndicatorsDriveDailyActivities:true, preserveSourceWording:true, integrationModel:"50% current-subject core achievement indicators + 50% levelled integration from other subjects"}),
+    status:function(){return {ready:true, productionApproved:false, externalValidationRequired:true};}
+  });
+})();
