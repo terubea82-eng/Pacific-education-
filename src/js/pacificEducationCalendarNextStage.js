663dad3b9b220b89ce25f49dd9749b3294b6e663
TERUBEA TION <terubea82@gmail.com>
2026-09-21 09:27:04 +1200

    Continue Pacific Education build: next-stage control module

---
+++ src/js/pacificEducationCalendarNextStage.js
@@ -0,0 +1,4 @@
+(function(){
+"use strict";
+window.PacificEducationCalendarNextStage=Object.freeze({version:"1.0.0",rules:Object.freeze({teacherDayOneManual:true, revisionAndExamDatesRedistributeRemainingActivities:true, examsOnlyOnCoveredIndicators:true, weekendsAndHolidaysSupported:true}),status:function(){return {ready:true,productionApproved:false,externalValidationRequired:true};}});
+})();
