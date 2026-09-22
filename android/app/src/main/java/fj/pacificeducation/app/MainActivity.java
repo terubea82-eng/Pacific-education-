c01e5f843ea8da8e0c0a87d17bbcaffd8a5a6744
TERUBEA TION <terubea82@gmail.com>
2026-09-22 16:26:02 +1200

    Add secure Android WebView launcher for Pacific Education pilot

---
+++ android/app/src/main/java/fj/pacificeducation/app/MainActivity.java
@@ -0,0 +1,116 @@
+package fj.pacificeducation.app;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public final class MainActivity extends Activity {
+    private static final String APP_ORIGIN = "https://terubea82-eng.github.io";
+    private static final String APP_URL =
+            "https://terubea82-eng.github.io/Pacific-education-/";
+
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        webView = new WebView(this);
+        configureWebView(webView);
+        setContentView(webView);
+
+        if (savedInstanceState == null) {
+            webView.loadUrl(APP_URL);
+        } else {
+            webView.restoreState(savedInstanceState);
+        }
+    }
+
+    private void configureWebView(WebView view) {
+        WebSettings settings = view.getSettings();
+        settings.setJavaScriptEnabled(true);
+        settings.setDomStorageEnabled(true);
+        settings.setDatabaseEnabled(false);
+        settings.setAllowFileAccess(false);
+        settings.setAllowContentAccess(false);
+        settings.setBuiltInZoomControls(false);
+        settings.setDisplayZoomControls(false);
+        settings.setMixedContentMode(WebSettings.MIXED_CONTENT_NEVER_ALLOW);
+        settings.setSafeBrowsingEnabled(true);
+        settings.setMediaPlaybackRequiresUserGesture(true);
+
+        view.setVerticalScrollBarEnabled(true);
+        view.setHorizontalScrollBarEnabled(false);
+        view.setBackgroundColor(0xFFFFFFFF);
+
+        view.setWebViewClient(new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+                return handleUrl(request.getUrl());
+            }
+
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                return handleUrl(Uri.parse(url));
+            }
+
+            @Override
+            public void onPageStarted(WebView view, String url, Bitmap favicon) {
+                view.setVisibility(View.VISIBLE);
+                super.onPageStarted(view, url, favicon);
+            }
+        });
+
+        WebView.setWebContentsDebuggingEnabled(BuildConfig.DEBUG);
+    }
+
+    private boolean handleUrl(Uri uri) {
+        if (uri == null) return true;
+
+        String scheme = uri.getScheme();
+        String host = uri.getHost();
+
+        if ("https".equalsIgnoreCase(scheme) && APP_ORIGIN.equalsIgnoreCase(
+                uri.getScheme() + "://" + host)) {
+            return false;
+        }
+
+        Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+        startActivity(intent);
+        return true;
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (webView != null && webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        if (webView != null) {
+            webView.saveState(outState);
+        }
+        super.onSaveInstanceState(outState);
+    }
+
+    @Override
+    protected void onDestroy() {
+        if (webView != null) {
+            webView.stopLoading();
+            webView.destroy();
+            webView = null;
+        }
+        super.onDestroy();
+    }
+}
