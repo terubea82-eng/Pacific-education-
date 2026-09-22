63145c1b9dc6e3330a298d03fa1c9df1249e3328
TERUBEA TION <terubea82@gmail.com>
2026-09-22 16:25:32 +1200

    Add Android app build configuration targeting API 36

---
+++ android/app/build.gradle.kts
@@ -0,0 +1,31 @@
+plugins {
+    id("com.android.application")
+}
+
+android {
+    namespace = "fj.pacificeducation.app"
+    compileSdk = 36
+
+    defaultConfig {
+        applicationId = "fj.pacificeducation.app"
+        minSdk = 24
+        targetSdk = 36
+        versionCode = 1
+        versionName = "1.0.0-pilot"
+    }
+
+    buildTypes {
+        debug {
+            applicationIdSuffix = ".debug"
+            versionNameSuffix = "-debug"
+        }
+        release {
+            isMinifyEnabled = true
+            isShrinkResources = true
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+}
